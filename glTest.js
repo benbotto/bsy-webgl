80c496ee025606e93fde4cb519e0afc57fb0eb14
benbotto <benbotto@users.noreply.github.com>
2017-11-04 10:02:36 -0700

    Working version.  Needs some refactoring in SquareRenderer.

--- glTest.js
+++ glTest.js
@@ -1,8 +1,8 @@
 (function(bsy) {
   'use strict';
 
-  const canvas = document.getElementById('easel');
-  const gl     = canvas.getContext('webgl');
+  const easel  = new bsy.Easel(640, 480);
+  const gl     = easel.getContext();
 
   // Vertex shader.
   const vsSource = `
@@ -31,131 +31,16 @@
   const program  = linker.link(gl, vShader, fShader);
 
   // Create the world.
-  const world = new bsy.World();
-  world.addWorldObject('square1', new bsy.Square(gl, program));
+  const world  = new bsy.World();
+  const square = new bsy.Square();
 
-  // Create a buffer for drawing.
-  // TODO: This should be done in a Square class.
-  const bufMgr   = new bsy.BufferMgr();
-  const buffer   = bufMgr.fillNewBuffer(gl, [
-    -0.5,   0.5, 0,
-     0.5,   0.5, 0,
-     0.5,  -0.5, 0,
+  world.addWorldObject('square1', square);
 
-     0.5,  -0.5, 0,
-    -0.5,  -0.5, 0,
-    -0.5,   0.5, 0,
-  ]);
+  // Create the renderers.
+  const worldRenderer = new bsy.WorldRenderer(gl, world, program)
+    .addRenderer(new bsy.SquareRenderer(gl, square, program));
 
-  // The main world renderer.
-  // TODO: The renderer has the projection, but the WorldObjects each has a
-  // program.  Since the projection matrix varies from program to program, what
-  // sets the projection?  Maybe there should be a ProgramMgr instance that has
-  // the "active" program?
-  const renderer = new bsy.Renderer(gl, world);
-  renderer.start();
-
-  // EVERYTHING BELOW HERE NEEDS REFACTORING.
-
-  const programInfo = {
-    program: program,
-    attribLocations: {
-      vertexPosition: gl.getAttribLocation(program, 'aVertexPosition'),
-    },
-    uniformLocations: {
-      projectionMatrix: gl.getUniformLocation(program, 'uProjectionMatrix'),
-      modelViewMatrix: gl.getUniformLocation(program, 'uModelViewMatrix'),
-    },
-  };
-
-  console.log(programInfo);
-
-
-  //drawScene(gl, programInfo, buffer);
-
-  function drawScene(gl, programInfo, buffer) {
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
-    gl.clearDepth(1.0);                 // Clear everything
-    gl.enable(gl.DEPTH_TEST);           // Enable depth testing
-    gl.depthFunc(gl.LEQUAL);            // Near things obscure far things
-
-    // Clear the canvas before we start drawing on it.
-
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-    // Create a perspective matrix, a special matrix that is
-    // used to simulate the distortion of perspective in a camera.
-    // Our field of view is 45 degrees, with a width/height
-    // ratio that matches the display size of the canvas
-    // and we only want to see objects between 0.1 units
-    // and 100 units away from the camera.
-
-    const fieldOfView = 45 * Math.PI / 180;   // in radians
-    const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
-    const zNear = 0.1;
-    const zFar = 100.0;
-    const projectionMatrix = mat4.create();
-
-    // note: glmatrix.js always has the first argument
-    // as the destination to receive the result.
-    mat4.perspective(projectionMatrix,
-                     fieldOfView,
-                     aspect,
-                     zNear,
-                     zFar);
-
-    // Set the drawing position to the "identity" point, which is
-    // the center of the scene.
-    const modelViewMatrix = mat4.create();
-
-    // Now move the drawing position a bit to where we want to
-    // start drawing the square.
-
-    mat4.translate(modelViewMatrix,     // destination matrix
-                   modelViewMatrix,     // matrix to translate
-                   [-0.0, 0.0, -6.0]);  // amount to translate
-
-    // Tell WebGL how to pull out the positions from the position
-    // buffer into the vertexPosition attribute.
-    {
-      const numComponents = 3;
-      const type = gl.FLOAT;
-      const normalize = false;
-      const stride = 0;
-      const offset = 0;
-      gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-      gl.vertexAttribPointer(
-          programInfo.attribLocations.vertexPosition,
-          numComponents,
-          type,
-          normalize,
-          stride,
-          offset);
-      gl.enableVertexAttribArray(
-          programInfo.attribLocations.vertexPosition);
-    }
-
-    // Tell WebGL to use our program when drawing
-
-    gl.useProgram(programInfo.program);
-
-    // Set the shader uniforms
-
-    gl.uniformMatrix4fv(
-        programInfo.uniformLocations.projectionMatrix,
-        false,
-        projectionMatrix);
-    gl.uniformMatrix4fv(
-        programInfo.uniformLocations.modelViewMatrix,
-        false,
-        modelViewMatrix);
-
-    {
-      const offset = 0;
-      const vertexCount = 6;
-      gl.drawArrays(gl.TRIANGLES, offset, vertexCount);
-    }
-  }
-
+  easel.onDraw = (gl, timeDeltaMS) => worldRenderer.render(gl, timeDeltaMS);
+  easel.start();
 })(window.bsy);
 
