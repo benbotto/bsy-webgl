a5374b4aaa0a04577408adacd509396921306748
benbotto <benbotto@users.noreply.github.com>
2017-11-04 08:05:09 -0700

    Added a class which manages the main view (the canvas).

---
+++ Easel.js
@@ -0,0 +1,58 @@
+(function(bsy) {
+  'use strict';
+
+  /**
+   * This class deals with the canvas and such (i.e. the easel is what the user
+   * draws on.
+   */
+  class Easel {
+    /**
+     * Initialize.
+     */
+    constructor(width, height) {
+      this.canvas        = window.document.createElement('canvas');
+      this.canvas.width  = width;
+      this.canvas.height = height;
+      this.gl            = this.canvas.getContext('webgl');
+      this.onDraw        = function () {};
+      this.startTime     = null;
+      this.lastTime      = null;
+      
+      window.document.body.appendChild(this.canvas);
+    }
+
+    /**
+     * Get the webgl context.
+     */
+    getContext() {
+      return this.gl;
+    }
+
+    /**
+     * Start drawing.
+     */
+    start() {
+      const self     = this;
+
+      this.startTime = new Date();
+      this.lastTime  = new Date();
+
+      window.requestAnimationFrame(doDraw);
+
+      function doDraw() {
+        // Time delta is needed on each render so that object can be moved
+        // correctly despite the speed of the device.
+        let curTime     = new Date();
+        let timeDeltaMS = curTime.getTime() - self.lastTime.getTime();
+        self.lastTime   = curTime;
+
+        self.onDraw(self.gl, timeDeltaMS);
+
+        window.requestAnimationFrame(doDraw);
+      }
+    }
+  }
+
+  bsy.Easel = Easel;
+})(window.bsy);
+
