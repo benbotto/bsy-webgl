a8c926a48b950ee8aaaaac2a2adf4c3efcb50012
benbotto <benbotto@users.noreply.github.com>
2017-11-04 10:36:10 -0700

    Separated model and view matrices.

--- glTest.js
+++ glTest.js
@@ -8,11 +8,12 @@
   const vsSource = `
     attribute vec4 aVertexPosition;
 
-    uniform mat4 uModelViewMatrix;
+    uniform mat4 uModelMatrix;
+    uniform mat4 uViewMatrix;
     uniform mat4 uProjectionMatrix;
 
     void main() {
-      gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+      gl_Position = uProjectionMatrix * uViewMatrix * uModelMatrix * aVertexPosition;
     }
   `;
 
