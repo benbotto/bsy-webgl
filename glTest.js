b3f6901c21a097dea1a25ba6b851e6bdd4511f24
benbotto <benbotto@users.noreply.github.com>
2017-11-04 20:34:26 -0700

    Now renders a cube.

--- glTest.js
+++ glTest.js
@@ -40,20 +40,35 @@
   // Create the world.
   const world  = new bsy.World();
   const square = new bsy.Square([1.0, 0.0, 1.0, 1.0])
-    .setLocation(vec3.fromValues(0, 0, -6));
+  const cube   = new bsy.Cube([
+    [0.0, 0.0, 1.0, 1.0],
+    [0.0, 1.0, 0.0, 1.0],
+    [0.0, 1.0, 1.0, 1.0],
+    [1.0, 0.0, 0.0, 1.0],
+    [1.0, 0.0, 1.0, 1.0],
+    [1.0, 1.0, 0.0, 1.0]
+  ]);
 
   world.addWorldObject('square1', square);
+  world.addWorldObject('cube1',   cube);
+
+  const squareTrans = square.getTransform();
+  mat4.translate(squareTrans, squareTrans, [0, 0, -6]);
+
+  const cubeTrans = cube.getTransform();
+  mat4.translate(cubeTrans, cubeTrans, [1.5, -1.5, -6]);
 
   // Create the renderers.
   const worldRenderer = new bsy.WorldRenderer(gl, world, program)
-    .addRenderer(new bsy.SquareRenderer(gl, square, program));
+    .addRenderer(new bsy.SquareRenderer(gl, square, program))
+    .addRenderer(new bsy.CubeRenderer(gl, cube, program));
 
   easel.onDraw = (gl, timeDeltaMS) => {
-    const loc  = square.getLocation();
-    const move = vec3.fromValues(.001, .001, -.001);
+    mat4.translate(squareTrans, squareTrans, [.001, .001, -.001]);
 
-    vec3.add(loc, loc, move);
-    square.setLocation(loc);
+    mat4.rotate(cubeTrans, cubeTrans, Math.PI * timeDeltaMS / 1000, [1.0, 0.0, 0.0]);
+    mat4.rotate(cubeTrans, cubeTrans, Math.PI * timeDeltaMS / 2000, [0.0, 1.0, 0.0]);
+    mat4.rotate(cubeTrans, cubeTrans, Math.PI * timeDeltaMS / 3000, [0.0, 0.0, 1.0]);
 
     worldRenderer.render(gl, timeDeltaMS);
   };
