01f5a9cb3a7735e50fc968c6baddb03aa83c1dd6
benbotto <benbotto@users.noreply.github.com>
2017-11-07 18:27:19 -0800

    Square with a texture.

---
+++ world-object/TextureSquare.js
@@ -0,0 +1,49 @@
+(function(bsy) {
+  'use strict';
+
+  /** A square with a texture. */
+  class TextureSquare extends bsy.Square {
+    /**
+     * Initialize the square.  The image must be loaded.
+     */
+    constructor(image) {
+      super();
+
+      this.setTextureImage(image);
+
+      this.textureCoords = [
+        0.0, 1.0,
+        1.0, 1.0,
+        1.0, 0.0,
+
+        1.0, 0.0,
+        0.0, 0.0,
+        0.0, 1.0
+      ];
+    }
+
+    /**
+     * Set the texture image.
+     */
+    setTextureImage(image) {
+      this.textureImage = image;
+    }
+
+    /**
+     * Get the texture image.
+     */
+    getTextureImage() {
+      return this.textureImage;
+    }
+
+    /**
+     * Get the texture coordinates.
+     */
+    getTextureCoords() {
+      return this.textureCoords;
+    }
+  }
+
+  bsy.TextureSquare = TextureSquare;
+})(window.bsy);
+
