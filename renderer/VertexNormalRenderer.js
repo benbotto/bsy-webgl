d3ce061bdd5cdc53620d5aa81fa7d69720551c2f
Ben Botto <benjamin.botto@gmail.com>
2017-11-15 17:48:49 -0800

    Added a renderer that renders vertex normals, which is nice for debugging new shapes.

---
+++ renderer/VertexNormalRenderer.js
@@ -0,0 +1,103 @@
+(function(bsy) {
+  'use strict';
+
+  /**
+   * Renders vertex normals.  The WorldObject must implement getVertices(),
+   * and getVertexNormals().
+   */
+  class VertexNormalRenderer extends bsy.Renderer {
+    /**
+     * Initialize the renderer.
+     */
+    constructor(gl, worldObj, program, buffMgr = new bsy.BufferMgr()) {
+      super(gl, worldObj, program);
+
+      this.vertices      = this.createVertexNormalLines(worldObj);
+      this.vertBuffer    = buffMgr.fillNewFloatArrayBuffer(gl, this.vertices);
+      this.modelLoc      = gl.getUniformLocation(program, 'uModelMatrix');
+      this.viewLoc       = gl.getUniformLocation(program, 'uViewMatrix');
+      this.projectionLoc = gl.getUniformLocation(program, 'uProjectionMatrix');
+      this.vertexLoc     = gl.getAttribLocation(program,  'aVertexPosition');
+    }
+
+    /**
+     * Create vertex normal line data.  These are the vertices that will be
+     * rendered.
+     */
+    createVertexNormalLines(worldObj) {
+      const verts   = toVec3Array(worldObj.getVertices());
+      const normals = toVec3Array(worldObj.getVertexNormals());
+
+      return verts
+        // Create a unit-length line from each vertex in the direction of the
+        // normal.  Returns two points.
+        .map((vert, i) => [vert, vec3.add(vec3.create(), vert, normals[i])])
+        // Flatten each set of vertices into one array.
+        .reduce((prev, cur) => prev.concat(cur), [])
+        // Flatten the vertices into one big array.
+        .reduce((prev, cur) => prev.concat(cur[0], cur[1], cur[2]), []);
+
+      // Helper to convert an array of floats to an array of vec3.
+      function toVec3Array(vecs) {
+        const vec3s = [];
+
+        for (let i = 0; i < vecs.length; i += 3)
+          vec3s.push(vec3.fromValues(vecs[i], vecs[i+1], vecs[i+2]));
+
+        return vec3s;
+      }
+    }
+
+    /**
+     * Get the projection location attribute.
+     */
+    getProjectionLoc() {
+      return this.projectionLoc;
+    }
+
+    /**
+     * Get the view location attribute.
+     */
+    getViewLoc() {
+      return this.viewLoc;
+    }
+
+    /**
+     * Get the model location attribute.
+     */
+    getModelLoc() {
+      return this.modelLoc;
+    }
+
+    /**
+     * Render the worldObj.
+     */
+    render(gl, timeDeltaMS, trans) {
+      this.useProgram();
+
+      // Locations.
+      gl.bindBuffer(gl.ARRAY_BUFFER, this.vertBuffer);
+      gl.vertexAttribPointer(this.vertexLoc, 3, gl.FLOAT, false, 0, 0);
+      gl.enableVertexAttribArray(this.vertexLoc);
+
+      // The model matrix is this object's transform and the parent's.
+      const modelMatrix = mat4.create();
+      mat4.multiply(modelMatrix, trans, this.getWorldObject().getTransform());
+      this.setModel(modelMatrix);
+
+      // Set the MVP in the shader.
+      this.writeProjection();
+      this.writeView();
+      this.writeModel();
+
+      // Draw the normal lines.
+      gl.drawArrays(gl.LINES, 0, this.vertices.length / 3);
+
+      // Cleanup.
+      gl.disableVertexAttribArray(this.vertexLoc);
+    }
+  }
+
+  bsy.VertexNormalRenderer = VertexNormalRenderer;
+})(window.bsy);
+
