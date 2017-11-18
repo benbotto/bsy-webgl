7d48339d76339bcbb173d666303e2cb30308ab9e
Ben Botto <benjamin.botto@gmail.com>
2017-11-17 17:36:18 -0800

    Now stores the face normal and has a getter for it.

--- world-object/Triangle.js
+++ world-object/Triangle.js
@@ -13,20 +13,29 @@
     constructor(p1, p2, p3) {
       super();
 
-      this.points        = [p1, p2, p3];
-      this.vertices      = bsy.VecUtils.flattenVec3Array(this.points);
-      this.indices       = [0, 1, 2];
-      this.vertexNormals = [];
+      this.points   = [p1, p2, p3];
+      this.vertices = bsy.VecUtils.flattenVec3Array(this.points);
+      this.indices  = [0, 1, 2];
 
-      for (let i = 0; i < this.points.length; ++i) {
-        let lastI = i - 1 < 0 ? 2 : i - 1;
-        let v1    = vec3.sub(vec3.create(), this.points[i], this.points[lastI]);
-        let v2    = vec3.sub(vec3.create(), this.points[i], this.points[(i + 1) % 3]);
+      // Compute the face normal.
+      // 1) Create two vectors from a single point on the triangle to the other
+      // two points.  It doesn't matter which point is used.
+      // 2) Take the cross product of those two vectors, which yields an
+      // orthagonal vector.  The order is important, so by the right-hand rule
+      // the points on the triangle need to be in clockwise order or the normal
+      // will point in the wrong direction.
+      const v1 = vec3.sub(vec3.create(), this.points[1], this.points[0]);
+      const v2 = vec3.sub(vec3.create(), this.points[1], this.points[2]);
 
-        this.vertexNormals.push(vec3.normalize(vec3.create(), vec3.cross(vec3.create(), v1, v2)));
-      }
+      this.faceNormal = vec3
+        .normalize(vec3.create(), vec3
+          .cross(vec3.create(), v1, v2));
 
-      this.vertexNormals = bsy.VecUtils.flattenVec3Array(this.vertexNormals);
+      // The vertex normals is just the face normal repeated once for each
+      // vertex.
+      this.vertexNormals = bsy.VecUtils
+        .flattenVec3Array(Array
+          .from({length: 3}, () => this.faceNormal));
     }
 
     /**
@@ -48,6 +57,13 @@
      */
     getVertexNormals() {
       return this.vertexNormals;
+    }
+
+    /**
+     * Get the face normal for the triangle.
+     */
+    getFaceNormal() {
+      return this.faceNormal;
     }
   }
 
