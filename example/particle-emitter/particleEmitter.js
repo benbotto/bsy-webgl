9dec9d67b30b06b24613b298712f51b8be996097
benbotto <benbotto@users.noreply.github.com>
2017-11-22 11:28:30 -0800

    Example of particle emitter.

---
+++ example/particle-emitter/particleEmitter.js
@@ -0,0 +1,57 @@
+(function(bsy) {
+  'use strict';
+
+  const easel  = new bsy.Easel();
+  const gl     = easel.getContext();
+
+  const NUM_PARTS = 3000;
+
+  // Compile and link the vertix and fragment shaders.
+  const compiler   = new bsy.Compiler();
+  const linker     = new bsy.Linker();
+
+  const peVShader = compiler.compile(gl, gl.VERTEX_SHADER, bsy.particleEmitterVert);
+  const peFShader = compiler.compile(gl, gl.FRAGMENT_SHADER, bsy.identityFrag);
+  const peProgram = linker.link(gl, peVShader, peFShader);
+
+  // Create the world.
+  const camera  = new bsy.SixDoFCamera();
+  const world   = new bsy.World(camera);
+  const emitter = new bsy.ParticleEmitter();
+
+  // Add the particles.
+  for (let i = 0; i < NUM_PARTS; ++i) {
+    const randColor = Array
+      .from({length: 3}, () => Math.random())
+      .concat(1);
+
+    const randVelocity = Array.from({length: 3}, () => Math.random() * 40 - 20);
+
+    const particle     = new bsy.Particle(new bsy.Circle(10))
+      .setColor(randColor)
+      .setVelocity(randVelocity)
+      .setLifetime(Math.random() * 5);
+
+    emitter.addWorldObject(`particle_${i}`, particle);
+  }
+
+  world.addWorldObject('emitter', emitter);
+
+  // Create the renderers.
+  const worldRenderer   = new bsy.WorldRenderer(gl, world)
+    .addRenderer(new bsy.ParticleEmitterRenderer(gl, emitter, peProgram));
+ 
+  // Position the camera.
+  camera.moveBackward(150);
+
+  easel.onDraw = (gl, timeDeltaMS) => {
+    worldRenderer.render(gl, timeDeltaMS);
+  };
+
+  // Update the canvas size with the window size is changed.
+  easel.onresize = () => worldRenderer.updateViewSize();
+
+  // Start rendering.
+  easel.start();
+})(window.bsy);
+
